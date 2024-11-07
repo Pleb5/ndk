5622335947ac969135adc4303d4120e937c26af3
daniyal <daniyal@nostrdev.com>
2024-11-07 19:58:27 +0500

    fix: check proofs validity before adding token to wallet

--- ndk-wallet/src/service/lifecycle/token.ts
+++ ndk-wallet/src/service/lifecycle/token.ts
@@ -2,13 +2,20 @@ import type { NDKEvent, NDKRelay } from "@nostr-dev-kit/ndk";
 import { NDKCashuToken } from "../../cashu/token";
 import type NDKWalletLifecycle from ".";
 import type { NDKCashuWallet } from "../../cashu/wallet";
+import { CashuMint, CashuWallet } from "@cashu/cashu-ts";
 
 async function handleToken(this: NDKWalletLifecycle, event: NDKEvent, relay?: NDKRelay) {
     if (this.knownTokens.has(event.id)) return;
     this.knownTokens.add(event.id);
 
     const token = await NDKCashuToken.from(event);
-    if (!token) return;
+    if (!token || !token.mint) return;
+
+    // check if token contains any spent proof
+    // if so then  discard this token
+    const _wallet = new CashuWallet(new CashuMint(token.mint));
+    const spentProofs = await _wallet.checkProofsSpent(token.proofs);
+    if (spentProofs.length > 0) return;
 
     const walletId = token.walletId;
     let wallet: NDKCashuWallet | undefined;
